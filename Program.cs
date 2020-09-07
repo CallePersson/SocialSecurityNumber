8361989aaae5925eaf13ec1ac38614cbd4d7e03f
Calle Persson <Calle_1997@hotmail.se>
2020-09-07 16:32:30 +0200

    adding generations

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Runtime.InteropServices;
 
 namespace SocialSecurityNumber
 {
@@ -7,20 +8,24 @@ namespace SocialSecurityNumber
     {
         static void Main(string[] args)
         {
-
-
-            Console.Write("Please enter Social Security Number (YYMMDD-XXXX)");
-
-            //deklarerar variabel + tilldelar den ett värde
-            string socialSecurityNumber = Console.ReadLine();
-
-
-            if (args.Length == 1)
+            string socialSecurityNumber;
+            string firstName = "not assigned";
+            string lastName = "not assigned";
+            if (args.Length != 0)
             {
-                socialSecurityNumber = args[0];
+                socialSecurityNumber = (args[0]);
+                
             }
-
-
+            else
+            {
+                Console.Write("Please enter your first name: ");
+                firstName = Console.ReadLine();
+                Console.Write("Please enter your last name: ");
+                lastName = Console.ReadLine();
+                Console.Write("Please enter Social Security Number (YYMMDD-XXXX)");
+                socialSecurityNumber = Console.ReadLine();
+            }
+            
             string genderNumberString = socialSecurityNumber.Substring(socialSecurityNumber.Length - 2, 1);
 
             int genderNumber = int.Parse(genderNumberString);
@@ -42,8 +47,37 @@ namespace SocialSecurityNumber
                 age--;
             }
 
-            Console.WriteLine($"{gender}, {age}");
+            //-------------------------------------------------------
+            string generation = "unknown";
 
+            if ((DateTime.Now.Year - age) <= 1945)
+            {
+                generation = "Silent Generation";
+            }
+            else if ((DateTime.Now.Year - age) >= 1946 && (DateTime.Now.Year - age) < 1965)
+            {
+                generation = "Baby Boomer";
+            }
+            else if ((DateTime.Now.Year - age) >= 1965 && (DateTime.Now.Year - age) < 1977)
+            {
+                generation = "Generation X";
+            }
+            else if ((DateTime.Now.Year - age) >= 1977 && (DateTime.Now.Year - age) < 1996)
+            {
+                generation = "Millenial";
+            }
+            else if ((DateTime.Now.Year - age) >= 1996)
+            {
+                generation = "Gen Z";
+            }
+          
+
+            //-------------------------------------------------------
+            Console.Clear();
+
+
+            Console.WriteLine($"Name:\t {firstName} {lastName} \r\nSocial Security Number: {socialSecurityNumber} \r\nGender: {gender}\r\nAge: {age}\r\nGeneration: {generation} ");
+                
 
         }
     }
